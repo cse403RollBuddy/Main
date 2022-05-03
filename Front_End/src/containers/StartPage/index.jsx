dfe35a0d712912f7eaac132a9bc63c961b0e9a15
Steve <spater1@uw.edu>
2022-05-03 12:53:26 -0700

    fixed bug in index.jsx

--- Front_End/src/containers/StartPage/index.jsx
+++ Front_End/src/containers/StartPage/index.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 export default function StartPage() {
   const [dummy, setDummy] = useState(null);
   const dummyCall = async () => {
-    await fetch("http:localhost:4567/data")
+    await fetch("http://localhost:4567/data")
       .then((response) => response.json())
       .catch((e) => {
         alert("Cant get to the server :(");
