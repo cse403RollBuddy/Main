36d88923b7f10b5b6b24fad0e8ba834c7fee9cd9
Steve <spater1@uw.edu>
2022-05-10 10:50:51 -0700

    Small updates: - added maps for races to ability score increases

--- src/main/java/Back_End/SparkServer.java
+++ src/main/java/Back_End/SparkServer.java
@@ -1,6 +1,8 @@
 package Back_End;
 import static spark.Spark.*;
 import com.google.gson.Gson;
+
+import java.util.HashMap;
 //import CORSFilter.java;
 //import ExampleDataTransfer.java;
 
@@ -33,12 +35,46 @@ public class SparkServer {
                           Integer.parseInt(req.params(":faces").trim()))
         );
 
+        get("/ability", (req, res) -> {
+            HashMap<String, String> abilities = new HashMap<>();
+            String race = req.queryParams("race").toUpperCase();
+            abilities.put("Strength", "0");
+            abilities.put("Dexterity", "0");
+            abilities.put("Constitution", "0");
+            abilities.put("Intelligence", "0");
+            abilities.put("Wisdom", "0");
+            abilities.put("Charisma", "0");
+            switch (race) {
+                case "HUMAN": abilities.put("Strength", "1");
+                              abilities.put("Dexterity", "1");
+                              abilities.put("Constitution", "1");
+                              abilities.put("Intelligence", "1");
+                              abilities.put("Wisdom", "1");
+                              abilities.put("Charisma", "1");
+                              break;
+                case "ELF":   abilities.put("Dexterity", "2");
+                              abilities.put("Wisdom", "1");
+                              break;
+                case "DWARF": abilities.put("Constitution", "2");
+                              abilities.put("Strength", "2");
+                              break;
+                case "HALFLING": abilities.put("Dexterity", "2");
+                                 abilities.put("Constitution", "1");
+                                 break;
+            }
+            return gson.toJson(abilities);
+        });
 
         get("/background", (req, res) -> gson.toJson("ACOLYTE CRIMINAL SOLDIER"));
 
         get("/classes", (req, res) -> gson.toJson("CLERIC FIGHTER ROGUE"));
 
         get("/races", (req, res) -> gson.toJson("HUMAN ELF DWARF HALFLING"));
+
+        get("/shutdown", (req, res) -> {
+            stop();
+            return "Server successfully shutdown.";
+        });
 
     }
 }
