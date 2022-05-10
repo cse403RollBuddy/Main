ee5d0b9be0ec3fab5b7467112a77f56a835f6738
Steve <spater1@uw.edu>
2022-05-10 11:44:34 -0700

    added some more paths to SparkServer.java

--- src/main/java/Back_End/SparkServer.java
+++ src/main/java/Back_End/SparkServer.java
@@ -13,6 +13,7 @@ public class SparkServer {
     public static void main (String[] args) {
 
         Gson gson = new Gson();
+        Character beingCreated = new Character();
 
         initExceptionHandler((e) -> System.out.println("Uh-oh"));
         CORSFilter corsFilter = new CORSFilter();
@@ -30,11 +31,41 @@ public class SparkServer {
                 "Hello: " + request.params(":name")
         );
 
+        // returns the result of rolling numDice dice, each of which is a faces-sided die
+        // e.g. /roll/4/6 returns a list which contains 4 random values from 1-6
         get("/roll/:numDice/:faces", (req, res) ->
                 Dice.Roll(Integer.parseInt(req.params(":numDice").trim()),
                           Integer.parseInt(req.params(":faces").trim()))
         );
 
+        // sets the character's name
+        get("/create", (req, res) -> {
+                    String param = req.queryParams("name");
+                    beingCreated.set_name(param);
+                    return 0;
+        });
+
+        get("/setclass", (req, res) -> {
+           String characterClass = req.queryParams("class").toUpperCase();
+           switch (characterClass) {
+               case "FIGHTER": beingCreated.set_class(Classes.FIGHTER); break;
+               case "CLERIC":  beingCreated.set_class(Classes.CLERIC); break;
+               case "ROGUE":   beingCreated.set_class(Classes.ROGUE); break;
+           }
+           return 0;
+        });
+
+        get("/setbackground", (req, res) -> {
+            String background = req.queryParams("background").toUpperCase();
+            switch (background) {
+                case "ACOLYTE":  beingCreated.set_background(Backgrounds.ACOLYTE);
+                case "CRIMINAL": beingCreated.set_background(Backgrounds.CRIMINAL);
+                case "SOLDIER":  beingCreated.set_background(Backgrounds.SOLDIER);
+            }
+            return 0;
+        });
+
+        // sets the character's race and returns a useful map to the frontend
         get("/ability", (req, res) -> {
             HashMap<String, Integer> abilities = new HashMap<>();
             String race = req.queryParams("race").toUpperCase();
@@ -51,26 +82,34 @@ public class SparkServer {
                               abilities.put("Intelligence", 1);
                               abilities.put("Wisdom", 1);
                               abilities.put("Charisma", 1);
+                              beingCreated.set_race(Races.HUMAN);
                               break;
                 case "ELF":   abilities.put("Dexterity", 2);
                               abilities.put("Wisdom", 1);
+                              beingCreated.set_race(Races.ELF);
                               break;
                 case "DWARF": abilities.put("Constitution", 2);
                               abilities.put("Strength", 2);
+                              beingCreated.set_race(Races.DWARF);
                               break;
                 case "HALFLING": abilities.put("Dexterity", 2);
                                  abilities.put("Constitution", 1);
+                                 beingCreated.set_race(Races.HALFLING);
                                  break;
             }
             return gson.toJson(abilities);
         });
 
+        // for the frontend to get the list of possible Backgrounds
         get("/background", (req, res) -> gson.toJson("ACOLYTE CRIMINAL SOLDIER"));
 
+        // for the frontend to get the list of possible Classes
         get("/classes", (req, res) -> gson.toJson("CLERIC FIGHTER ROGUE"));
 
+        // for the frontend to get the list of possible Races
         get("/races", (req, res) -> gson.toJson("HUMAN ELF DWARF HALFLING"));
 
+        // shuts down the server
         get("/shutdown", (req, res) -> {
             stop();
             return "Server successfully shutdown.";
